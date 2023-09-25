8da9e54b11017a016f8b7b64f0c609c829b02156
Sriya Mukkawar <mukkawarsriya@gmail.com>
2023-09-25 14:26:36 +0530

    Add files via upload

---
+++ backend/models/userModel.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+
+const userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Please add a Name']
+    },
+    email: {
+        type: String,
+        required: [true, 'Please add an email']
+    },
+    password: {
+        type: String,
+        required: [true, 'Please add a Password']
+    },
+}, {
+    timestamps: true
+})
+
+module.exports = mongoose.model('User', userSchema)
---
+++ backend/routes/goalRoutes.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const router = express.Router()
+const { getGoals, setGoals, UpdateGoals, deleteGoals } = require('../controllers/goalController')
+const { protect } = require('../middleware/authMiddleware')
+
+router.route('/').get(protect, getGoals).post(protect, setGoals)
+router.route('/:id').put(protect, UpdateGoals).delete(protect, deleteGoals)
+
+module.exports = router
